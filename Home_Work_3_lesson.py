303249bd2ccdef258a17a21bdd6a9e512c391c89
Denis <denial03061984@gmail.com>
2021-04-08 14:46:35 +0300

    add Home_Work_3_lesson.py

---
+++ Home_Work_3_lesson.py
@@ -0,0 +1,26 @@
+# первая версия решения
+
+valeo = 50
+new_valeo = valeo/2
+if valeo < 100:
+    print(new_valeo)
+else:
+    print(valeo-(valeo*2))
+
+#вторая версия решения
+
+new_valeo = valeo/2 if valeo < 100 else (valeo-(valeo*2))
+print(new_valeo)
+
+##################################################################################################
+
+
+valeo = 50
+new_valeo = valeo/2
+if valeo < 100:
+     print(new_valeo)
+else:
+     print(valeo-(valeo*2))
+
+
+
