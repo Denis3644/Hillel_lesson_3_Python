553338ee19cd5f0aaa69fe9498a7972f9d9f40e4
Denis <denial03061984@gmail.com>
2021-04-08 15:28:20 +0300

    add Home_Work_3_lesson.py

--- Home_Work_3_lesson.py
+++ Home_Work_3_lesson.py
@@ -1,6 +1,6 @@
+#Задание 1
 # первая версия решения
-
-valeo = 50
+valeo = 130
 new_valeo = valeo/2
 if valeo < 100:
     print(new_valeo)
@@ -13,14 +13,43 @@ new_valeo = valeo/2 if valeo < 100 else (valeo-(valeo*2))
 print(new_valeo)
 
 ##################################################################################################
-
-
-valeo = 50
-new_valeo = valeo/2
+#Задание 2
+# первая версия решения
+valeo = 90
+new_valeo = valeo - valeo + 1
 if valeo < 100:
      print(new_valeo)
 else:
-     print(valeo-(valeo*2))
+     print(valeo-valeo)
 
+#вторая версия решения
+new_valeo = valeo - valeo + 1 if valeo < 100 else valeo-valeo
+print(new_valeo)
+
+
+########################################################################################################
+#Задание 3
+# первая версия решения
+valeo = 5
+new_valeo = True
+if valeo < 100:
+     print(new_valeo)
+else:
+     print(False)
+
+#вторая версия решения
+new_valeo = True if valeo < 100 else False
+print(new_valeo)
+
+########################################################################################################
+#Задание 3
+# Нечетные
+my_str = "my_str"
+print(my_str[1::2])
+########################################################################################################
+#Задание 4
+# Четные
+my_str = "my_str"
+print(my_str[2::2])
 
 
