cc9db328d826812aca5f09b911c3c6b92710d435
Denis <denial03061984@gmail.com>
2021-04-11 21:41:57 +0300

    add Home_Work_4_lesson.py

--- Home_Work_4_lesson.py
+++ Home_Work_4_lesson.py
@@ -1,39 +1,34 @@
 
-# my_list= [1,223,14,564,245,2,345,18]
-# for value in my_list:
-#      if value > 100:
-#           print(value)
+my_list= [1,223,14,564,245,2,345,18]
+for value in my_list:
+     if value > 100:
+          print(value)
 
 ###################################################
-# my_list1 = [1,5,223,14,564,245,2,345,18,67877]
-# my_results = []
-# for my_results in my_list1:
-#     if my_results > 100:
-#         print(my_results)
+my_list1 = [1,5,223,14,564,245,2,345,18,67877]
+my_results = []
+for my_results in my_list1:
+    if my_results > 100:
+        print(my_results)
 ######################################################
 
 
-# my_list = [66,78,76]
-# value = 5
-# if value <23:
-#     my_list.append(0)
-# else:
-#     new_valeo = my_list[-1] + my_list[-2]
-#     my_list.append(new_valeo)
-# print(my_list)
+my_list = [66,78,76]
+value = 5
+if value <23:
+    my_list.append(0)
+else:
+    new_valeo = my_list[-1] + my_list[-2]
+    my_list.append(new_valeo)
+print(my_list)
 
-
-
+#######################################################
+rus=[]
 my_str = "0123456789"
 for value1 in my_str:
     for value2 in my_str:
-        print(value1 + value2)
-
-
-
-
-
-
+        rus.append(int(value1+value2))
+print(rus)
 
 
 
